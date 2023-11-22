37a1660f4826be52e88394a9548bd5f4ca112340
Zheng, Corey <czheng45@gatech.edu>
2023-11-22 16:22:17 -0500

    Added saving and loading
    - Added saving and loading of model params functionality to the trainer,
    
    - Changed the test_loop function to evaluate(). Also removed all references towards "accuaracy" calucations in the train/val/test, since there is no classification task.

--- src/utils.py
+++ src/utils.py
@@ -1,5 +1,6 @@
 import math
 
+import os
 import torch
 from torch.utils.data import DataLoader
 import torch.nn as nn
@@ -8,7 +9,7 @@ from torch.optim import Optimizer
 
 class Trainer:
     def __init__(self, dataLoader: DataLoader, valData: DataLoader, model: nn.Module, loss_fn: nn.Module,
-                 optimizer: Optimizer, saveDirectory, config = None):
+                 optimizer: Optimizer, saveDirectory, config=None):
         self.trainLoader = dataLoader
         self.valLoader = valData
         self.model = model
@@ -16,23 +17,27 @@ class Trainer:
         self.optimizer = optimizer
         self.saveDirectory = saveDirectory
 
-
         self.train_loss_history = []
         self.val_loss_history = []
         self.bestepoch = 0  # epoch of the currently best model
 
-        #if the user did not present a config, initialize default config
+        # if the user did not present a config, initialize default config
         if config is None:
             config["epochs"] = 100
             config["verbose"] = True
             config["doplot"] = True
             config["saveincrement"] = 25
 
-        self.epochs = config["epochs"] #total number epochs
-        self.verbose = config["verbose"] #Verbose training status updates?
-        self.doplot = config["doplot"] #output epoch plots?
-        self.saveincrement = config["saveincrement"] #save model weights every 25 epochs
+        self.epochs = config["epochs"]  # total number epochs
+        self.verbose = config["verbose"]  # Verbose training status updates?
+        self.doplot = config["doplot"]  # output epoch plots?
+        self.saveincrement = config["saveincrement"]  # save model weights every 25 epochs
 
+    # initialize a new trainer by loading in the parameters from a model weight file
+    # @classmethod
+    # def loadModelTrainer(cls, modelname):
+
+    # Single Epoch training loop
     def train_epoch(self):
         self.model.train()
         avgloss = 0;
@@ -41,27 +46,30 @@ class Trainer:
             pred = self.model(inputs)
             loss = self.criterion(pred, gt)
 
-
             loss.backward()
             self.optimizer.step()
 
-            #update avg loss of this epoch
+            # update avg loss of this epoch
             avgloss += loss
 
-            #Verbose progress monitor
+            # Verbose progress monitor
             if self.verbose:
                 if batch % 100 == 0:
                     print(f"batch {batch + 1} loss: {loss:>7f}")
 
         # record loss history
-        avgloss = avgloss/(batch+1)
+        avgloss = avgloss / (batch + 1)
         self.train_loss_history.append(avgloss)
         return avgloss
 
-    def train(self)
+    # Training the model over multiple epochs
+    def train(self, epochs=None):
+
+        if epochs is None:
+            epochs = self.epochs
 
         bestloss = math.inf
-        for epoch in range(self.epochs):
+        for epoch in range(epochs):
             print("------------------------------------------------")
             print(f"Epoch {epoch + 1}:")
             trainloss = self.train_epoch()
@@ -70,17 +78,23 @@ class Trainer:
             vloss = self.validate()
             print(f"Validation Loss {vloss}:")
 
-            #Save the best model
+            # Save the best model
             if vloss < bestloss:
                 self.bestepoch = epoch
+                self.save_model('bestmodel.pth')
 
+            # Save model every "saveincrement" epochs
+            if (epoch+1) % self.saveincrement == 0:
+                self.save_model('epoch' + str(epoch) + '.pth')
 
-        print("Done!")
+        print("Training Done!")
 
-
+    # Validation against validation dataset
     def validate(self):
         self.model.eval()
         avgloss = 0
+
+        # no grad for validation
         with torch.no_grad():
             for i, (inputs, gt) in enumerate(self.valLoader):
                 pred = self.model(inputs)
@@ -89,23 +103,29 @@ class Trainer:
                 # update avg validation loss
                 avgloss += loss
 
-        avgloss = avgloss/(i+1)
-        self.val_loss_history.append(avgloss)
-        return avgloss
+            avgloss = avgloss / (i + 1)
+            self.val_loss_history.append(avgloss)
+            return avgloss
+
+    # Save the current model parameters
+    def save_model(self, fname):
+        outPath = self.saveDirectory + '/' + fname
+        torch.save(self.model.state_dict(), outPath)
+
+    # Load a model parameters into the trainer model
+    def load_model(self, filedir):
+        self.model.load_state_dict(torch.load(filedir))
+        self.model.eval()
 
 
-
-def test_loop(dataloader: DataLoader, model: nn.Module, loss_fn: nn.Module):
+def evaluate(dataloader: DataLoader, model: nn.Module, loss_fn: nn.Module):
     model.eval()
-    size = len(dataloader.dataset)
     num_batches = len(dataloader)
-    test_loss, correct = 0, 0
+    test_loss = 0
     with torch.no_grad():  # to make sure no gradient is calculated
         for input, label in dataloader:
-            # this code was mostly copied from the pytorch tutorial. I don't know why they didn't use enumerate or if num_batches is different from size.
             pred = model(input)
             test_loss += loss_fn(pred, label).item()
-            correct += (pred.argmax(1) == label).type(torch.float).sum().item()
+
         test_loss /= num_batches
-        correct /= size
-        print(f"Test Error: \n Accuracy: {(100 * correct):>0.1f}%, Avg loss: {test_loss:>8f} \n")
+        print(f"Avg loss: {test_loss:>8f} \n")
