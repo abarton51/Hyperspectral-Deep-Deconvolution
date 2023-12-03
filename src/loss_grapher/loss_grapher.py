5a9603459d25c54889743c3e9dd498e7e665cff9
mtaher3 <mtaher3@gatech.edu>
2023-12-02 21:19:52 -0500

    Added higher quality image

--- src/loss_grapher/loss_grapher.py
+++ src/loss_grapher/loss_grapher.py
@@ -1,11 +1,12 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-model_path = 'I:\Georgia Institute of Technology\Deep Learning Project Group - General\saved_models\ReducedUNet'
+# model_path = 'I:\Georgia Institute of Technology\Deep Learning Project Group - General\saved_models\ReducedUNet'
+model_path = 'ClassicUnet/'
 
-train_path = model_path + '\\trainloss.txt'
-val_path = model_path + '\\valloss.txt'
-image_path = model_path + '\\loss_curve.png'
+train_path = model_path + 'trainloss.txt'
+val_path = model_path + 'valloss.txt'
+image_path = model_path + 'loss_curve.png'
 
 train_loss = np.loadtxt(train_path)
 val_loss = np.loadtxt(val_path)
@@ -20,4 +21,4 @@ plt.ylabel("Loss")
 
 plt.xlim(0, 1000)
 
-plt.savefig(image_path)
+plt.savefig(image_path, dpi=1000)
